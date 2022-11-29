ab49100ba47adbb4b1bfdad3e3c8ca05890822a8
LuisaRojasH <luisarojas182@hotmail.com>
2022-11-29 11:40:55 -0500

    añade carpeta components y archivos login y register

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
+/* eslint-disable import/no-cycle */
+import { login } from './components/login.js';
+import { register } from './components/register.js';
+
+const rootDiv = document.getElementById('root');
+
+const routes = {
+  '/login': login,
+  '/register': register,
+};
+
+export const onNavigate = (pathname) => {
+  window.history.pushState(
+    {},
+    pathname,
+    window.location.origin + pathname,
+  );
+  rootDiv.appendChild(routes[pathname]());
+};
+
+const component = routes[window.location.pathname];
+
+rootDiv.appendChild(component);
+
 // Este es el punto de entrada de tu aplicacion
-
-import { myFunction } from './lib/index.js';
-
-myFunction();
+// import { myFunction } from './lib/index.js';
+// myFunction();
