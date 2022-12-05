e6323f3bc369775ddf7c021e4aef075b1d57a78a
LuisaRojasH <luisarojas182@hotmail.com>
2022-12-05 15:37:43 -0500

    aañade formulario de registro y estructura en css

--- src/components/login.js
+++ src/components/login.js
@@ -1,16 +1,26 @@
 export const login = (onNavigate) => {
 // div cuerpo login
   const homeDiv = document.createElement('div');
-  homeDiv.className = 'divHome';
+  homeDiv.className = 'homeDivLogin';
   // div contenedor de inputs
   const inputDiv = document.createElement('div');
   inputDiv.className = 'divInputs';
   homeDiv.appendChild(inputDiv);
+  // titulo usuario o email
+  const userEmailTitle = document.createElement('h3');
+  userEmailTitle.className = 'userEmailTilte';
+  userEmailTitle.textContent = 'Correo Electronico';
+  inputDiv.appendChild(userEmailTitle);
   // input usuario
   const inputMail = document.createElement('input');
   inputMail.className = 'inputMail';
   inputMail.textContent = 'Correo Electronico o Nombre de Usuario';
   inputDiv.appendChild(inputMail);
+  // titulo password
+  const passwordTitle = document.createElement('h3');
+  passwordTitle.className = 'passwordTilte';
+  passwordTitle.textContent = 'Contraseña';
+  inputDiv.appendChild(passwordTitle);
   // input contraseña
   const inputPassword = document.createElement('input');
   inputPassword.className = 'inputPassword';
@@ -20,16 +30,16 @@ export const login = (onNavigate) => {
   const divSignUpLogin = document.createElement('div');
   divSignUpLogin.className = 'divSingUpLogin';
   homeDiv.appendChild(divSignUpLogin);
-  // boton ir al registro
-  const btnSignUp = document.createElement('button');
-  btnSignUp.className = 'btnSingUp';
-  btnSignUp.textContent = 'Regístrate';
-  divSignUpLogin.appendChild(btnSignUp);
   // boton logearse
   const btnLogin = document.createElement('button');
   btnLogin.className = 'btnLogin';
   btnLogin.textContent = 'Inicia Sesión';
   divSignUpLogin.appendChild(btnLogin);
+  // boton ir al registro
+  const btnSignUp = document.createElement('button');
+  btnSignUp.className = 'btnSingUp';
+  btnSignUp.textContent = 'Regístrate';
+  divSignUpLogin.appendChild(btnSignUp);
 
   btnSignUp.addEventListener('click', () => onNavigate('/register'));
 
--- src/components/register.js
+++ src/components/register.js
@@ -1,10 +1,60 @@
 export const register = (onNavigate) => {
   // div general
   const homeDiv = document.createElement('div');
-  homeDiv.textContent = 'Bienvenido al Registro';
-
+  homeDiv.className = 'homeDivRegister';
+  // titulo registro
+  const registerTitle = document.createElement('h3');
+  registerTitle.className = 'registerTitle';
+  registerTitle.textContent = 'Bienvenido al Registro';
+  homeDiv.appendChild(registerTitle);
+  // div contenedor formulario de registro
+  const divInput = document.createElement('div');
+  divInput.className = 'divInputRegister';
+  homeDiv.appendChild(divInput);
+  // titulo nombre de usuaro
+  const userNameTitle = document.createElement('p');
+  userNameTitle.className = 'userNameTitle';
+  userNameTitle.textContent = 'Nombre de ususario';
+  divInput.appendChild(userNameTitle);
+  // input nombre de usuario
+  const inputAddUserName = document.createElement('input');
+  inputAddUserName.className = 'inputAddUserName';
+  divInput.appendChild(inputAddUserName);
+  // titulo correo electronico
+  const emailTitle = document.createElement('p');
+  emailTitle.className = 'emailTitle';
+  emailTitle.textContent = 'Correo Electronico';
+  divInput.appendChild(emailTitle);
+  // input correo
+  const inputAddMail = document.createElement('input');
+  inputAddMail.className = 'inputAddMail';
+  divInput.appendChild(inputAddMail);
+  // titulo contraseña
+  const passwordTitle = document.createElement('p');
+  passwordTitle.className = 'passwordTitle';
+  passwordTitle.textContent = 'Contraseña';
+  divInput.appendChild(passwordTitle);
+  // input contraseña
+  const inputAddPassword = document.createElement('input');
+  inputAddPassword.className = 'inputAddPassword';
+  divInput.appendChild(inputAddPassword);
+  // titulo contraseña
+  const confirmPassword = document.createElement('p');
+  confirmPassword.className = 'confirmPassword';
+  confirmPassword.textContent = 'Confirmar Contraseña';
+  divInput.appendChild(confirmPassword);
+  // input confirmar contraseña
+  const inputConfirmPassword = document.createElement('input');
+  inputConfirmPassword.className = 'inputConfirmPassword';
+  divInput.appendChild(inputConfirmPassword);
+  // boton registrarme
+  const btnregister = document.createElement('button');
+  btnregister.className = 'btnRegisterMe';
+  btnregister.textContent = 'Registrarme';
+  homeDiv.appendChild(btnregister);
   // boton regresar al home
   const btnHome = document.createElement('button');
+  btnHome.className = 'btnbackHome';
   btnHome.textContent = 'Regresar al Home';
   homeDiv.appendChild(btnHome);
 
