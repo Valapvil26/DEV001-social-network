9e772ce32515061eba9a4c65cabdc3408c2e77c4
Heberlis <heberlisfinol@gmail.com>
2023-01-12 15:33:32 -0300

    Añade actualizacion de test

--- test/firebase.spec.js
+++ test/firebase.spec.js
@@ -3,7 +3,7 @@ import {
   signInWithPopup, signOut, updateProfile, // GoogleAuthProvider,
 } from 'firebase/auth';
 import {
-// auth, // collection, addDoc, getDocs, onSnapshot, deleteDoc, doc, getDoc, updateDoc, orderBy,
+  addDoc, collection, deleteDoc, onSnapshot, getDoc, updateDoc, // doc
 } from 'firebase/firestore';
 // import { app, db } from '../src/firebase/firebaseconfig.js';
 
@@ -12,13 +12,15 @@ import {
   updateName,
   signIn,
   signInGoogle,
-  // addPost,
-  // onGetPost,
-  // deletePost,
-  // getPost,
-  // updatePost,
+  addPost,
+  onGetPost,
+  deletePost,
+  getPost,
+  updatePost,
   logOut,
+  auth,
 } from '../src/firebase/firebase.js';
+import { db } from '../src/firebase/firebaseconfig.js';
 
 jest.mock('firebase/auth');
 jest.mock('firebase/firestore');
@@ -77,9 +79,72 @@ describe('updateName', () => {
     expect(typeof updateName).toBe('function');
   });
   it('ejecuta updateProfile', async () => {
-    const userName = 'usuario prueba';
+    const userName = { displayName: 'Fulanito' };
     await updateName(userName);
-    expect(userName).toBe('usuario prueba');
+    expect(userName).toBe('Fulanito');
     expect(updateProfile).toHaveBeenCalled();
+  });
+});
+
+describe('addPost', () => {
+  it('deberia ser una funcion', () => {
+    expect(typeof addPost).toBe('function');
+  });
+  it('ejecuta addDoc ', async () => {
+    const post = {
+      date: '10/1/2023, 13:39:48',
+      post: 'comentario de prueba 3500',
+      userId: 'hfTOVE6NyjXKfz5wsyxjhN3aAXH2',
+      userName: 'luisaRojasH',
+
+    };
+    await addPost(collection(db, 'posts'), post);
+    expect(addDoc).toHaveBeenCalled();
+  });
+});
+
+describe('onGetPost', () => {
+  it('deberia ser una funcion', () => {
+    expect(typeof onGetPost).toBe('function');
+  });
+  it('ejecuta onSnapshot', async () => {
+    onGetPost(collection(db, 'posts'));
+    expect(onSnapshot).toHaveBeenCalled();
+  });
+});
+
+describe('deletePost', () => {
+  it('deberia ser una funcion', () => {
+    expect(typeof deletePost).toBe('function');
+  });
+  it('ejecuta deleteDoc', async () => {
+    const id = 'hfTOVE6NyjXKfz5wsyxjhN3aAXH2';
+    deletePost(id);
+    expect(deleteDoc).toHaveBeenCalled();
+  });
+});
+
+describe('getPost', () => {
+  it('deberia ser una funcion', () => {
+    expect(typeof getPost).toBe('function');
+  });
+  it('ejecuta getDoc', async () => {
+    const id = 'hfTOVE6NyjXKfz5wsyxjhN3aAXH2';
+    getPost(id);
+    expect(getDoc).toHaveBeenCalled();
+  });
+});
+
+describe('updatePost', () => {
+  it('deberia ser una funcion', () => {
+    expect(typeof updatePost).toBe('function');
+  });
+  it('ejecuta getDoc', async () => {
+    const id = 'hfTOVE6NyjXKfz5wsyxjhN3aAXH2';
+    const newfields = {
+      post: 'Editado',
+    };
+    updatePost(id, newfields);
+    expect(updateDoc).toHaveBeenCalled();
   });
 });
